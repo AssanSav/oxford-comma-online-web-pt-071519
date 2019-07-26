1dc6b24bd9e91f2fcdb06fd6e29134ff72c4a4b9
assane savadogo <assane.savadogo81@gmail.com>
2019-07-26 04:23:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  return array.join if array.size == 1 
+  if array.size == 2 
+    array.insert(1, "and").join(" ")
+    elsif array.size >= 3 
+    array[-1] = "and #{array[-1]}"
+    array.join(", ")
+  end
 end
