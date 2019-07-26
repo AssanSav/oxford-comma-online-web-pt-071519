98bd557dcd351ef85d66f06bbe6a7a49f78000f2
assane savadogo <assane.savadogo81@gmail.com>
2019-07-26 04:24:20 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def oxford_comma(array)
   return array.join if array.size == 1 
   if array.size == 2 
@@ -5,5 +6,6 @@ def oxford_comma(array)
     elsif array.size >= 3 
     array[-1] = "and #{array[-1]}"
     array.join(", ")
+    binding.pry
   end
 end
